5fc1c67fc0ba72d4b6d6d299075dff3e238c8d21
NaseerAhmed3 <118593959+NaseerAhmed3@users.noreply.github.com>
2023-03-17 21:41:07 +0500

    updated

--- lib/adminpage.dart
+++ lib/adminpage.dart
@@ -76,160 +76,158 @@ class _AdminpageState extends State<Adminpage> {
       body: Container(
         height: double.infinity,
         width: double.infinity,
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 5),
-                child: Container(
-                  height: 50,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: InkWell(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const Signup()),
-                            );
-                          },
-                          child: Container(
-                            width: 150.0,
-                            decoration: BoxDecoration(
-                              color: Colors.red,
-                              borderRadius: BorderRadius.circular(30),
-                            ),
-                            child: Center(
-                              child: Text(
-                                "Empolyees",
-                                style: TextStyle(fontSize: 25),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: Container(
-                          width: 150.0,
-                          decoration: BoxDecoration(
-                            color: Colors.red,
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                          child: Center(
-                            child: Text(
-                              "Vehicals",
-                              style: TextStyle(fontSize: 25),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: InkWell(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const Signin()),
-                            );
-                          },
-                          child: Container(
-                            width: 150.0,
-                            decoration: BoxDecoration(
-                              color: Colors.red,
-                              borderRadius: BorderRadius.circular(30),
-                            ),
-                            child: Center(
-                              child: Text(
-                                "Customer",
-                                style: TextStyle(fontSize: 25),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: Container(
-                          width: 150.0,
-                          decoration: BoxDecoration(
-                            color: Colors.red,
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                          child: Center(
-                            child: Text(
-                              "Payment Info",
-                              style: TextStyle(fontSize: 25),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 0),
-                child: Container(
-                  height: 700,
-                  width: double.infinity,
-                  child: GridView.count(
-                    primary: false,
-                    padding: const EdgeInsets.all(20),
-                    crossAxisSpacing: 10,
-                    mainAxisSpacing: 10,
-                    crossAxisCount: 2,
-                    children: <Widget>[
-                      InkWell(
-                        child: Container(
-                          padding: const EdgeInsets.all(8),
-                          color: Colors.teal[100],
-                          child: const Text("He'd have you all unravel at the"),
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(8),
-                        color: Colors.teal[200],
-                        child: const Text('Heed not the rabble'),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(8),
-                        color: Colors.teal[300],
-                        child: const Text('Sound of screams but the'),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(8),
-                        color: Colors.teal[400],
-                        child: const Text('Who scream'),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(8),
-                        color: Colors.teal[500],
-                        child: const Text('Revolution is coming...'),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      InkWell(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 5),
+              child: Container(
+                height: 50,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: InkWell(
                         onTap: () {
-                          Navigator.pushNamed(context, '/maponly');
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const Signup()),
+                          );
                         },
                         child: Container(
-                          height: 300,
-                          width: double.infinity,
-                          color: Color.fromARGB(255, 139, 185, 223),
+                          width: 150.0,
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Empolyees",
+                              style: TextStyle(fontSize: 25),
+                            ),
+                          ),
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Container(
+                        width: 150.0,
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "Vehicals",
+                            style: TextStyle(fontSize: 25),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const Signin()),
+                          );
+                        },
+                        child: Container(
+                          width: 150.0,
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Customer",
+                              style: TextStyle(fontSize: 25),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Container(
+                        width: 150.0,
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "Payment Info",
+                            style: TextStyle(fontSize: 25),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              )
-            ],
-          ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 0),
+              child: Container(
+                height: 600,
+                width: double.infinity,
+                child: GridView.count(
+                  primary: false,
+                  padding: const EdgeInsets.all(20),
+                  crossAxisSpacing: 30,
+                  mainAxisSpacing: 30,
+                  crossAxisCount: 2,
+                  children: <Widget>[
+                    InkWell(
+                      child: Container(
+                        padding: const EdgeInsets.all(8),
+                        color: Colors.teal[100],
+                        child: const Text("He'd have you all unravel at the"),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      color: Colors.teal[200],
+                      child: const Text('Heed not the rabble'),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      color: Colors.teal[300],
+                      child: const Text('Sound of screams but the'),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      color: Colors.teal[400],
+                      child: const Text('Who scream'),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      color: Colors.teal[500],
+                      child: const Text('Revolution is coming...'),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        Navigator.pushNamed(context, '/maponly');
+                      },
+                      child: Container(
+                        height: 300,
+                        width: double.infinity,
+                        color: Color.fromARGB(255, 139, 185, 223),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
